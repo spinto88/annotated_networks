10cf4f6cc780d224d2af14f6880e44d569b72c70
Sena Facu <spinto@df.uba.ar>
2018-08-13 19:11:12 -0300

    archivos py agregados

---
+++ network.py
@@ -0,0 +1,31 @@
+import ctypes 
+
+class Edge(ctypes.Structure):
+
+	_fields_ = [('target', ctypes.c_int), 
+		('weight', ctypes.c_double)]
+
+class Vertex(ctypes.Structure):
+
+	_fields_ = [('id', ctypes.c_int),
+		('degree', ctypes.c_int),
+		('label', ctypes.POINTER(c_char)),
+		('edge', ctypes.POINTER(Edge))]
+
+class Network(ctypes.Structure):
+
+	_fields_ = [('nvertices', ctypes.c_int),
+		('directed', ctypes.c_int),
+		('vertex', ctypes.POINTER(Vertex))]
+
+	# Read GML file
+	def readgml(self, file_name):
+		pass
+
+	def community_
+
+
+
+
+
+
